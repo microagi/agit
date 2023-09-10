dacc610473993770f4229bd2b1774996b947b94f
Sivan Grunberg <sivang@gmail.com>
2023-09-10 23:44:17 +0300

    fix license

--- agit/main.py
+++ agit/main.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
+# microAGI, AGit Tool is dually licensed:
+#
+# microAGI tools are open sourced and free to use under GPLv3 in a non-profit
+# context (individual, and non-profit endeavors). If you plan to use it in commercial
+# setting please contact us at <contact@vitakka.co> for details and pricing.
+#
+# The GPLv3 governs individual, non-profit use.
+#
 # Copyright (C) 2023 Sivan Grünberg <sivan@vitakka.co>
 # Vitakka Consulting https://vitakka.co
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
+
 import asyncio
 from logging import DEBUG
 import openai
