c164644a3268dbf5ce5004198aee99e9b575eebf
Sivan Grunberg <sivan@vitakka.co>
2023-09-30 21:09:26 +0300

    major bugfix release 0.0.5 see changelog

---
+++ tests/test_os_iface.py
@@ -0,0 +1,9 @@
+import pytest
+from agit.os_iface import normalize
+
+
+def test_normalize():
+    # Test a non-destructive command
+    # TODO: Test the new syntactic PEG support by spec'ing complex destructive commands
+    result = normalize("git log -3 --pretty=format:'%h %s'")
+    assert result == ['git', 'log', '-3', '--pretty=format:%h %s']
