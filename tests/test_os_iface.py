3d622a753d169456c43990a223a9b7c86974cd42
Sivan Grunberg <sivan@vitakka.co>
2023-10-06 15:11:18 +0300

    black run

--- tests/test_os_iface.py
+++ tests/test_os_iface.py
@@ -9,4 +9,4 @@ def test_normalize():
     assert result == ["git", "log", "-3", "--pretty=format:%h %s"]
 
     result = handle_pipe("git log --format='%aN' | sort -u")
-    assert result == (["git", "log", "--format=%aN"] , ["sort", "-u"])
+    assert result == (["git", "log", "--format=%aN"], ["sort", "-u"])
