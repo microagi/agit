2035bccf455bb27877c11bf2bec0225efb845c92
Sivan Grunberg <sivan@vitakka.co>
2023-10-06 15:10:22 +0300

    add a test for regression purpose

--- tests/test_os_iface.py
+++ tests/test_os_iface.py
@@ -1,5 +1,5 @@
 import pytest
-from agit.os_iface import normalize
+from agit.os_iface import normalize, handle_pipe
 
 
 def test_normalize():
@@ -7,3 +7,6 @@ def test_normalize():
     # TODO: Test the new syntactic PEG support by spec'ing complex destructive commands
     result = normalize("git log -3 --pretty=format:'%h %s'")
     assert result == ["git", "log", "-3", "--pretty=format:%h %s"]
+
+    result = handle_pipe("git log --format='%aN' | sort -u")
+    assert result == (["git", "log", "--format=%aN"] , ["sort", "-u"])
